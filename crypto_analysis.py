ce4dc5b1e17d100588cc2b17ff7f49dd1ea4c2e5
Yitong Liu <yitongl@users.noreply.github.com>
2018-01-08 17:15:10 +0100

    adding try except

--- crypto_analysis.py
+++ crypto_analysis.py
@@ -5,34 +5,39 @@ import subprocess
 
 # Show all columns on one line when printing
 pd.set_option('display.width', 1000)
-coinmarketcap = Market()
+try:
+    coinmarketcap = Market()
 
-has_pinged = []
+    has_pinged = []
 
-while True:
-    all_currencies = coinmarketcap.ticker()
+    while True:
+        all_currencies = coinmarketcap.ticker()
 
-    df = pd.DataFrame(all_currencies)
-    df = df.convert_objects(convert_numeric=True)
+        df = pd.DataFrame(all_currencies)
+        df = df.convert_objects(convert_numeric=True)
 
-    sorted_1h = df.sort_values(by = ['percent_change_1h'], ascending=False)
-    
-    print('------------------------------------------------------------------------------------')
-    print('Most increase in value during the last hour')
-    print(sorted_1h.head(10)[['symbol', 'price_usd', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']])
-    print('------------------------------------------------------------------------------------')
-    print('Most decrease in value during the last hour')
-    print(sorted_1h.tail(10)[['symbol', 'price_usd', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']])
-    print('------------------------------------------------------------------------------------')
-    
-    
-    increase_10pct = sorted_1h[sorted_1h['percent_change_1h'] > 10.0]['symbol'].values
-    
-    for value in increase_10pct:
-        if value not in has_pinged:
-            subprocess.call(['afplay', '/System/Library/Sounds/Ping.aiff'])            
-            print (value)
-    
-    has_pinged.extend(increase_10pct)
+        sorted_1h = df.sort_values(by = ['percent_change_1h'], ascending=False)
+        
+        print('------------------------------------------------------------------------------------')
+        print('Most increase in value during the last hour')
+        print(sorted_1h.head(10)[['symbol', 'price_usd', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']])
+        print('------------------------------------------------------------------------------------')
+        print('Most decrease in value during the last hour')
+        print(sorted_1h.tail(10)[['symbol', 'price_usd', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d']])
+        print('------------------------------------------------------------------------------------')
+        
+        
+        increase_10pct = sorted_1h[sorted_1h['percent_change_1h'] > 10.0]['symbol'].values
+        
+        for value in increase_10pct:
+            if value not in has_pinged:
+                subprocess.call(['afplay', '/System/Library/Sounds/Ping.aiff'])            
+                print (value)
+        
+        has_pinged.extend(increase_10pct)
 
-    sleep(300)
+        sleep(300)
+
+except:
+    print("Unexpected error:")
+
